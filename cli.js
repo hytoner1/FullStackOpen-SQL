3ce9dc916ec5d3d61c85a2ec052fe058c0d08fca
Roni Hytonen <roni.hytonen93@gmail.com>
2021-12-23 12:59:35 +0200

    Finish 13.3. Took 15min

--- cli.js
+++ cli.js
@@ -11,17 +11,21 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 })
 
 const main = async () => {
+  let blogs = [];
   try {
     console.log('Establishing connection.')
     await sequelize.authenticate()
     console.log('Connection has been established successfully.')
 
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
-    console.log(blogs)
+    blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
     sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
+
+  blogs.forEach(b => {
+    console.log(`${b.author}: '${b.title}', ${b.likes} likes`);
+  });
 }
 
 main()
